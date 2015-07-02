79b2ce96b6995c1469a57533ffd679595b59ce17
WOONGKYU HAM <wg1107@naver.com>
2015-07-02 22:33:20 +0900

    1st_assignment
    :)

---
+++ 1st_assignment.R
@@ -0,0 +1,19 @@
+# SKKU ISS 2015, Lecture 4
+# Objective: plotting open government data
+
+# Jevin West
+# Date:  7/2/2015
+
+#clean variables
+rm(list=ls(all=TRUE))
+
+# read in data
+
+D <- read.csv("D:/assignment/2015.ISS/Data Science/7.2/HW/BC Health - PharmaCare Beneficiaries by Age Group, 2010-11.csv")
+
+a1 <- D[D$Population>20000,]
+
+hist(a1$Beneficiaries)
+
+plot(a1$Percentage.of.Age.Group.Receiving.Benefits)
+     
